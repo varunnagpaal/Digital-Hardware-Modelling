791277fa92b0aa78238cb94d13ece9f42b794cb3
Varun Nagpal <varun.nagpal.courses@gmail.com>
2019-12-06 21:09:19 -0600

    simplified multiple master xi stream with 2 HP pors but strange warnings

--- xilinx-vitis-hls/experiments/Example.cpp
+++ xilinx-vitis-hls/experiments/Example.cpp
@@ -3,44 +3,33 @@
 #define UNROLL_FACTOR 40
 #define	DATA_SZ		  (UNROLL_FACTOR*100)
 
+const int unroll_factor = UNROLL_FACTOR;
+
+void task(volatile ap_uint<16> *stream, ap_uint<16> (&buff)[DATA_SZ])
+{
+#pragma HLS ARRAY_PARTITION variable=buff cyclic factor=unroll_factor dim=1
+	memcpy(buff,(const ap_uint<16>*)stream,DATA_SZ*sizeof(ap_uint<16>));
+
+	stream_process_loop:
+	for(int i=0; i < DATA_SZ; i++){
+		#pragma HLS PIPELINE
+		#pragma HLS UNROLL factor=unroll_factor
+		buff[i] = buff[i] * 100;
+	}
+
+	memcpy((ap_uint<16> *)stream,buff,DATA_SZ*sizeof(ap_uint<16>));
+}
+
 void example(volatile ap_uint<16> *streamA, volatile ap_uint<16> *streamB)
 {
 #pragma HLS DATAFLOW
-	const int unroll_factor = UNROLL_FACTOR;
-
 #pragma HLS INTERFACE m_axi depth=unroll_factor port=streamA bundle=hp0 offset=slave
 #pragma HLS INTERFACE m_axi depth=unroll_factor port=streamB bundle=hp2 offset=slave
 #pragma HLS INTERFACE s_axilite port=return
 
-int i;
 ap_uint<16> buffA[DATA_SZ];
-#pragma HLS ARRAY_PARTITION variable=buffA cyclic factor=40 dim=1
 ap_uint<16> buffB[DATA_SZ];
-#pragma HLS ARRAY_PARTITION variable=buffB cyclic factor=40 dim=1
 
-	{
-		memcpy(buffA,(const ap_uint<16>*)streamA,DATA_SZ*sizeof(ap_uint<16>));
-
-		stream1_process_loop:
-		for(i=0; i < DATA_SZ; i++){
-			#pragma HLS PIPELINE
-			#pragma HLS UNROLL factor=unroll_factor
-			buffA[i] = buffA[i] * 100;
-		}
-
-		memcpy((ap_uint<16> *)streamA,buffA,DATA_SZ*sizeof(ap_uint<16>));
-	}
-
-	{
-		memcpy(buffB,(const ap_uint<16>*)streamB,DATA_SZ*sizeof(ap_uint<16>));
-
-		stream2_process_loop:
-		for(i=0; i < DATA_SZ; i++){
-			#pragma HLS PIPELINE
-			#pragma HLS UNROLL factor=unroll_factor
-			buffB[i] = buffB[i] * 100;
-		}
-
-		memcpy((ap_uint<16> *)streamB,buffB,DATA_SZ*sizeof(ap_uint<16>));
-	}
+	task( streamA, buffA);
+	task( streamB, buffB);
 }
